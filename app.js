e6a194f513469f840518c3c7baefe6122a437734
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-06-20 20:39:42 +0530

    added basic styles

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -17,6 +18,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.engine('ejs',ejsMate);
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
@@ -41,7 +43,7 @@ app.post('/campgrounds', async (req, res) => {
 
 app.get('/campgrounds/:id', async (req, res) => {
     const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', { campground })
+    res.render('campgrounds/show', { campground });
 });
 
 app.get('/campgrounds/:id/edit', async (req, res) => {
--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const axios = require('axios');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground')
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp');
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -13,18 +14,36 @@ db.once("open", () => {
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
 
+//image generator
+
+async function image() {
+    try {
+        const resp = await axios.get('https://api.unsplash.com/photos/random', {
+            params: {
+                client_id: 'cRSDgb9tCmDExFpAFFOVcs9PFnTCkdsIvpgtkGrn_5A',
+                collections: 1114848,
+            },
+        })
+        return resp.data.urls.small
+    } catch (err) {
+        console.error(err)
+    }
+};
+
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i=0; i<50;i++) {
+    for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state }`,
-            title:`${sample(descriptors)} ${sample(places)}`
-        })
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,image: await image() , description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae iusto cumque mollitia expedita commodi voluptas ullam, atque quis quaerat ratione quidem vero, rem vel voluptatibus nemo. Dolor explicabo eos iusto!',
+            price
+        });
         await camp.save();
     }
 }
 
 seedDB().then(() => {
-    mongoose.conection.close();
+    mongoose.connection.close();
 })
