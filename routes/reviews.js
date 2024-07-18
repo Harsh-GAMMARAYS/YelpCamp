8768464f07d725db88733c95092cdf634f61a5be
Harsh-GAMMARAYS <gammaray.coding@gmail.com>
2024-07-19 05:19:28 +0530

    Added a bit more styling

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,24 +4,10 @@ const catchAsync = require('../utils/catchAsync');
 const { validateReview, isLoggedIn, isReviewAuthor} = require('../middleware');
 const Review = require('../models/review');
 const Campground = require('../models/campground');
+const reviews = require('../controllers/reviews');
 
-router.post('/', validateReview, isLoggedIn, catchAsync(async (req, res, next) => {
-    const campground = await Campground.findById(req.params.id);
-    const review = new Review(req.body.review);
-    review.author = req.user._id;
-    campground.reviews.push(review);
-    await review.save();
-    await campground.save();
-    req.flash('success', "New review Created")
-    res.redirect(`/campgrounds/${campground._id}`);
-}));
+router.post('/', validateReview, isLoggedIn, catchAsync(reviews.createReview));
 
-router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res, next) => {
-    const {id, reviewId} = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash('success','Successfully Deleted Review')
-    res.redirect(`/campgrounds/${id}`);
-}))
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
 
 module.exports = router;
